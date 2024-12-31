b7610b8818ba7d5af125e09b48065ba1189ce67c
haily <49448330+melly8954@users.noreply.github.com>
2024-12-31 23:33:28 +0900

    241231 Spring Security 초안

---
+++ src/main/java/com/melly/myjpa/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.melly.myjpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
+                .authorizeHttpRequests(auth ->
+                                auth.anyRequest().permitAll()); // 모든 사용자가 접근할 수 있도록 허용
+
+        return http.build();
+
+    }
+}
