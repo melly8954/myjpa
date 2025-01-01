802f17d4544407330da950ec3468729c422c047d
haily <49448330+melly8954@users.noreply.github.com>
2025-01-01 21:08:30 +0900

    250101  Role Entity 추가

---
+++ src/main/java/com/melly/myjpa/domain/RoleEntity.java
@@ -0,0 +1,21 @@
+package com.melly.myjpa.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name="role_tbl")
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String role;
+
+}
