ef1111fa76aaf6590877db90857020cdcbbfd80f
haily <49448330+melly8954@users.noreply.github.com>
2025-01-02 01:22:18 +0900

    250102 화면 controller 추가

--- src/main/java/com/melly/myjpa/controller/UserController.java
+++ src/main/java/com/melly/myjpa/controller/UserController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
     @GetMapping("/login")
     public String registerForm() {
         return "login_security/login_form";
