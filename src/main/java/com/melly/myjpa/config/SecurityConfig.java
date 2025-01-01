d55220a377b092841ab147ff4d49bd21f4b018a4
haily <49448330+melly8954@users.noreply.github.com>
2025-01-01 19:37:23 +0900

    250101 config 간단 변경

--- src/main/java/com/melly/myjpa/config/SecurityConfig.java
+++ src/main/java/com/melly/myjpa/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity  // Spring Security 필터가 Spring의 필터 체인에 등록되고, 애플리케이션의 보안 구성이 가능해진다. / Spring Security의 웹 보안을 활성화
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,8 +21,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
-                .authorizeHttpRequests(auth ->
-                                auth.anyRequest().permitAll()); // 모든 사용자가 접근할 수 있도록 허용
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()); // 모든 사용자가 접근할 수 있도록 허용
 
         return http.build();
 
