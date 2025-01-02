084941f84ec7276b884ce683c95382f7fd0951cc
haily <49448330+melly8954@users.noreply.github.com>
2025-01-03 05:53:39 +0900

    250103 OAuth 의 provider 등 다른 컬럼 추가

--- src/main/java/com/melly/myjpa/domain/UserEntity.java
+++ src/main/java/com/melly/myjpa/domain/UserEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import java.util.Date;
 
 @Getter
 @NoArgsConstructor
@@ -24,4 +25,21 @@ public class UserEntity {
     @ManyToOne
     @JoinColumn(name="role_id")
     private RoleEntity role;
+
+    // OAuth를 위해 구성한 추가 필드 2개
+    private String provider;
+    @Column(name="provider_id")
+    private String providerId;
+
+
+    // createDate 필드 추가
+    @Column(name="create_date")
+    private Date createDate;
+    // 엔티티가 영속화되기 전에 현재 시간을 자동으로 설정하는 메서드
+    @PrePersist
+    public void prePersist() {
+        if (this.createDate == null) {
+            this.createDate = new Date();  // 현재 시간을 설정
+        }
+    }
 }
