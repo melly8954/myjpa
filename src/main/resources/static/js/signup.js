5f096121de9c1f1eaf28a0ae3eb318263691acf9
haily <49448330+melly8954@users.noreply.github.com>
2025-01-04 13:20:22 +0900

    250104 회원가입 유효성 검사 마무리

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -3,7 +3,12 @@ $("#submit").on("click", function (event) {
     event.preventDefault(); // 기본 폼 전송 막기
 
     // 각 유효성 검사 함수 호출
-    if (!validatePassword() || !validateEmailDuplicate() || !validateEmailVerification()) {
+    if( !validateLoginId() ||
+        !validatePassword() ||
+        !validateName() ||
+        !validateNickname() ||
+        !validateEmailDuplicate() ||
+        !validateEmailVerification() ){
         return; // 유효하지 않으면 함수 종료
     }
 
@@ -41,59 +46,156 @@ $("#submit").on("click", function (event) {
     });
 });
 
+// 로그인 ID 중복 확인 버튼 클릭 이벤트
+$('#check-loginId-button').on('click', function () {
+    validateLoginId();
+});
+
+// 로그인 ID 유효성 검사
+function validateLoginId() {
+    let loginId = $('#loginId').val();
+
+    // 클라이언트에서 최소 길이 체크
+    if (loginId.length < 5 || loginId.length > 20) {
+        displayValidationMessage('#loginId', '로그인 ID는 5~20자 사이여야 합니다.', 'error');
+        return false;
+    }
+
+    // 서버에서 중복 확인
+    let isValid = false;
+    $.ajax({
+        type: 'POST',
+        url: '/api/users/check-loginId',
+        contentType: 'application/json',
+        data: JSON.stringify({ loginId: loginId }),
+        async: false, // 동기 처리
+        success: function (response) {
+            if (response) {
+                displayValidationMessage('#loginId', '로그인 ID가 이미 존재합니다.', 'error');
+                isValid = false;
+            } else {
+                displayValidationMessage('#loginId', '사용 가능한 로그인 ID입니다.', 'success');
+                isValid = true;
+            }
+        },
+        error: function () {
+            displayValidationMessage('#loginId', '로그인 ID 확인 중 오류가 발생했습니다. 다시 시도해주세요.', 'error');
+            isValid = false;
+        }
+    });
+
+    return isValid;
+}
+
 // 비밀번호 유효성 검사
 function validatePassword() {
     const password = $('#password').val();
     const confirmPassword = $('#confirmPassword').val();
+
+    // 비밀번호 입력 여부 확인
     if (password === '') {
         displayValidationMessage('#password', '비밀번호를 입력해주세요.', 'error');
         return false;
-    } else if (password !== confirmPassword) {
+    }
+
+    // 비밀번호 길이 확인
+    if (password.length < 8 || password.length > 20) {
+        displayValidationMessage('#password', '비밀번호는 8~20자 사이여야 합니다.', 'error');
+        return false;
+    }
+
+    // 비밀번호와 확인 비밀번호가 일치하는지 확인
+    if (password !== confirmPassword) {
         displayValidationMessage('#password', '비밀번호가 일치하지 않습니다.', 'error');
         return false;
+    }
+
+    // 모든 조건이 통과되면 유효한 비밀번호로 판단
+    displayValidationMessage('#password', '비밀번호가 유효합니다.', 'success');
+    return true;
+}
+
+// 이름 유효성 검사
+function validateName() {
+    const name = $('#name').val();
+    if (name.length < 2 || name.length > 100) {
+        displayValidationMessage('#name', '이름은 2~100자 사이여야 합니다.', 'error');
+        return false;
     } else {
-        displayValidationMessage('#password', '비밀번호가 유효합니다.', 'success');
+        displayValidationMessage('#name', '이름이 유효합니다.', 'success');
         return true;
     }
+}
+
+// 닉네임 중복 확인 버튼 클릭 이벤트
+$('#check-nickname-button').on('click', function () {
+    validateNickname();
+});
+
+// 닉네임 유효성 검사
+function validateNickname() {
+    let nickname = $('#nickname').val();
+
+    // 닉네임 최소 길이 및 최대 길이 체크
+    if (nickname.length < 5 || nickname.length > 30) {
+        displayValidationMessage('#nickname', '닉네임은 5~30자 사이여야 합니다.', 'error');
+        return false;
+    }
+
+    // 서버에서 중복 확인
+    let isValid = false;
+    $.ajax({
+        type: 'POST',
+        url: '/api/users/check-nickname',
+        contentType: 'application/json',
+        data: JSON.stringify({ nickname: nickname }),
+        async: false, // 동기 처리
+        success: function (response) {
+            if (response) {
+                displayValidationMessage('#nickname', '닉네임이 이미 존재합니다.', 'error');
+                isValid = false;
+            } else {
+                displayValidationMessage('#nickname', '사용 가능한 닉네임입니다.', 'success');
+                isValid = true;
+            }
+        },
+        error: function () {
+            displayValidationMessage('#nickname', '닉네임 확인 중 오류가 발생했습니다. 다시 시도해주세요.', 'error');
+            isValid = false;
+        }
+    });
+
+    return isValid;
 }
 
 // 이메일 중복 유효성 검사
 function validateEmailDuplicate() {
     if ($('#emailCheckMessage').hasClass('error')) {
-        displayValidationMessage('#email', '이메일 중복 확인이 필요합니다.', 'error');
+        displayValidationMessage('#email', 'email 인증이 필요합니다.', 'error');
         return false;
     } else {
-        displayValidationMessage('#email', '사용 가능한 이메일입니다.', 'success');
+        displayValidationMessage('#email', '사용 가능한 email 입니다.', 'success');
         return true;
     }
-}
-
-// 이메일 인증 상태 체크
-function validateEmailVerification() {
-    if (!$('#verificationMessage').hasClass('success')) {
-        alert('이메일 인증을 완료해주세요.');
-        return false;
-    }
-    return true;
-}
-
-// 유효성 검사 메시지 표시 함수
-function displayValidationMessage(inputId, message, status) {
-    const span = $('<span>').addClass('validation-message').text(message);
-    if (status === 'error') {
-        span.css('color', 'red');
-    } else {
-        span.css('color', 'green');
-    }
-
-    // 기존의 메시지가 있다면 제거하고 새로운 메시지로 갱신
-    $(inputId).next('.validation-message').remove();
-    $(inputId).after(span);
 }
 
 // 이메일 중복 검사
 $('#check-email-button').on('click', function() {
     let email = $('#email').val();
+
+    const emailRegex = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
+    // 이메일 형식 검사
+    if (!emailRegex.test(email)) {
+        displayValidationMessage('#email', '올바른 이메일 형식을 입력해주세요.', 'error');
+        return false;
+    }
+
+    // 클라이언트에서 최소 길이 체크
+    if (email.length < 10 || email.length > 255) {
+        displayValidationMessage('#email', 'email 의 길이는 10~255자 사이여야 합니다.', 'error');
+        return false;
+    }
 
     $.ajax({
         type: 'POST',
@@ -102,17 +204,17 @@ $('#check-email-button').on('click', function() {
         data: JSON.stringify({ mail: email }),
         success: function(response) {
             if (response) {
-                $('#emailCheckMessage').text("아이디가 이미 존재합니다.").removeClass('success').addClass('error');
-                displayValidationMessage('#email', '이메일 중복이 확인되었습니다.', 'error');
+                $('#emailCheckMessage').removeClass('success').addClass('error');
+                displayValidationMessage('#email', 'email 중복이 확인되었습니다.', 'error');
             } else {
-                $('#emailCheckMessage').text("사용 가능한 아이디입니다.").removeClass('error').addClass('success');
-                displayValidationMessage('#email', '사용 가능한 이메일입니다.', 'success');
+                $('#emailCheckMessage').removeClass('error').addClass('success');
+                displayValidationMessage('#email', '사용 가능한 email 입니다.', 'success');
             }
             validateEmailDuplicate();
         },
         error: function(error) {
-            $('#emailCheckMessage').text('이메일 확인 중 오류가 발생했습니다. 다시 시도해주세요.').removeClass('success').addClass('error');
-            displayValidationMessage('#email', '이메일 확인 중 오류가 발생했습니다.', 'error');
+            $('#emailCheckMessage').removeClass('success').addClass('error');
+            displayValidationMessage('#email', 'email 확인 중 오류가 발생했습니다.', 'error');
             validateEmailDuplicate();
         }
     });
@@ -122,7 +224,7 @@ $('#check-email-button').on('click', function() {
 $('#send-code-button').on('click', function() {
     // 이메일 중복 확인을 했는지 확인
     if (!$('#emailCheckMessage').hasClass('success')) {
-        alert('이메일 중복 확인을 진행하지 않으면 코드가 정상적으로 발송되지 않습니다.');
+        alert('email 중복 확인을 진행하지 않으면 코드가 정상적으로 발송되지 않습니다.');
         return; // 이메일 중복 확인을 하지 않았으면 코드 발송을 중단
     }
 
@@ -138,15 +240,24 @@ $('#send-code-button').on('click', function() {
         data: JSON.stringify({ mail: email }),
         success: function(response) {
             $('#verifyCodeSection').show();
-            alert('인증 메일이 발송되었습니다. 인증 번호를 확인해주세요.');
+            alert('인증 email 이 발송되었습니다. 인증 번호를 확인해주세요.');
             $('#loading-spinner').hide(); // 로딩 스피너 숨기기
         },
         error: function(error) {
-            alert('메일 발송에 실패했습니다. 다시 시도해주세요.');
+            alert('email 발송에 실패했습니다. 다시 시도해주세요.');
             $('#loading-spinner').hide(); // 로딩 스피너 숨기기
         }
     });
 });
+
+// 이메일 인증 상태 체크
+function validateEmailVerification() {
+    if (!$('#verificationMessage').hasClass('success')) {
+        alert('email 인증을 완료해주세요.');
+        return false;
+    }
+    return true;
+}
 
 // 인증 코드 확인
 $('#verify-code-button').on('click', function() {
@@ -176,3 +287,17 @@ $('#verify-code-button').on('click', function() {
         }
     });
 });
+
+// 유효성 검사 메시지 표시 함수
+function displayValidationMessage(inputId, message, status) {
+    const span = $('<span>').addClass('validation-message').text(message);
+    if (status === 'error') {
+        span.css('color', 'red');
+    } else {
+        span.css('color', 'green');
+    }
+
+    // 기존의 메시지가 있다면 제거하고 새로운 메시지로 갱신
+    $(inputId).next('.validation-message').remove();
+    $(inputId).after(span);
+}
